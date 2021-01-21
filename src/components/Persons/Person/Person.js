7f5788997780f87216035577095221d2f52c1bb0
akash1729 <aakash.aggarwal03@gmail.com>
2021-01-21 14:19:31 +0530

    Rendering adjacent jsx elements using HOC

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,6 @@
 import React , {Component} from 'react';
 import './Person.css';
-
+import Auxillary from '../../../HOC/Auxillary';
 
 
 class Person extends Component {
@@ -9,10 +9,13 @@ class Person extends Component {
     {
         console.log('[Person.js] rendering . .. ')
         return (
-            
-                [<p key="1A" onClick={this.props.click}>This is a {this.props.name}!! And he is {this.props.age} years old</p>,
-                <p key="1B">{this.props.children}</p>,
-                <input key="1C" type="text"  onChange= {this.props.NameChange} value={this.props.name} />]
+
+                <Auxillary>
+                    <p  onClick={this.props.click}>This is a {this.props.name}!! And he is {this.props.age} years old</p>
+                    <p>{this.props.children}</p>
+                    <input  type="text"  onChange= {this.props.NameChange} value={this.props.name} />
+                </Auxillary>
+                
 
 
             
